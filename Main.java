8962f94b9cbf2f0d3c6710b16a63b072d28d86c3
Wait-Loading <142930422+Wait-Loading@users.noreply.github.com>
2024-05-17 01:35:27 -0400

    j

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main 
-{
-    public static void main(String[] args) 
-    {
-       OS.Startup(new HelloWorld());//We start the OS by giving it a process to run
-        OS.CreateProcess(new GoodbyeWorld());//We create another process in the OS 
-    }
-}
